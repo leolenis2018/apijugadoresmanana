9f46e7d7fe303667d4a9b9f4c8f47af54a71412d
leolenis2018 <leolenis2018@gmail.com>
2021-10-29 06:36:03 -0500

    Se crea api REST

---
+++ models/ServidorModelo.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const cors = require('cors')
+
+//IMPORTAR LA FUNCION PARA CONECTARME CON LA BD
+const { conectarBD } = require('../database/conexion.js')
+
+const rutas = require('../routes/rutas.js')
+
+class ServidorModelo {
+
+    constructor() {
+
+        this.app = express();
+        this.despertarBD();
+        this.llamarAuxiliares();
+        this.enrutarPeticiones();
+
+    }
+
+    despertarServidor() {
+
+        this.app.listen(process.env.PUERTO, function () {
+            console.log("servidor encendido " + process.env.PUERTO)
+        })
+
+    }
+
+    enrutarPeticiones() {
+
+        this.app.use("/", rutas);
+
+    }
+
+    despertarBD() {
+
+        conectarBD()
+
+    }
+
+    llamarAuxiliares() {
+
+        this.app.use(express.json()) //Este habilita
+        this.app.use(cors())
+
+    }
+
+}
+
+module.exports = ServidorModelo
